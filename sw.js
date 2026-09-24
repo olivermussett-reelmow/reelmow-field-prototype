c0844c6db0526337eaf187957765991c5a90ade8
olivermussett-reelmow <olivermussett@gmail.com>
2026-09-24 06:35:48 -0400

    Add Garage service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="reelmow-garage-v1";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>cached)))});
